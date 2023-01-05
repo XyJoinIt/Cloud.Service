6050ad71d9eba433ccd800e65bffab8821e9adee
Riva <2377853907@qq.com>
2023-01-05 20:36:13 +0800

    up

---
+++ Cloud.Infra.Redis/Extensions/CloudRedisExtension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Cloud.Infra.Redis.Extensions;
+
+public static class CloudRedisExtension
+{
+    public static IServiceCollection AddCloudRedisService(this IServiceCollection service, Action<RedisOptions> option)
+    {
+
+        return service;
+    }
+}
+
+public class RedisOptions
+{
+    /// <summary>
+    /// 连接字符串
+    /// </summary>
+    public string RedisStrConn { get; set; }
+}
+
