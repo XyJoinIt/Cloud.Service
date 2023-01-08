5000580e522d34e9569b30c22617628bb7c86252
Riva <2377853907@qq.com>
2023-01-08 19:00:06 +0800

    up

---
+++ Cloud.Platform.Model/Migrations/20230108084608_v1.0.1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cloud.Platform.Model.Migrations
+{
+    /// <inheritdoc />
+    public partial class v101 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SysRole",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Sort = table.Column<int>(type: "int", nullable: false),
+                    Remark = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    EditId = table.Column<long>(type: "bigint", nullable: true),
+                    EditTime = table.Column<DateTime>(type: "datetime", nullable: true),
+                    CreateId = table.Column<long>(type: "bigint", nullable: false),
+                    CreateTime = table.Column<DateTime>(type: "datetime", nullable: false),
+                    DeleteId = table.Column<long>(type: "bigint", nullable: true),
+                    DeleteTime = table.Column<DateTime>(type: "datetime", nullable: true),
+                    IsDelete = table.Column<bool>(type: "tinyint(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SysRole", x => x.Id);
+                },
+                comment: "角色表")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SysRole");
+        }
+    }
+}
--- Cloud.Platform.Model/PlatformDbContextFactory.cs
+++ Cloud.Platform.Model/PlatformDbContextFactory.cs
@@ -12,13 +12,15 @@ namespace Cloud.Platform.Model
     {
         public PlatformDbContext CreateDbContext(string[] args)
         {
-            IConfigurationRoot configuration = new ConfigurationBuilder()
-                            .SetBasePath(Directory.GetCurrentDirectory())
-                            .AddJsonFile("appsettings.json")
-                            .Build();
-            var db = configuration.GetSection("ConnectionStrings:PlatformDb").Value;
+            // IConfigurationRoot configuration = new ConfigurationBuilder()
+            //                 .SetBasePath(Directory.GetCurrentDirectory())
+            //                 .AddJsonFile("appsettings.json")
+            //                 .Build();
+            // var db = configuration.GetSection("ConnectionStrings:PlatformDb").Value;
+            var dbcon =
+                "server=43.143.112.229;userid=YdProject;pwd=5JQAv^#v@vH&ic@1d;database=XyPlatform;connectiontimeout=3000;port=31124;Pooling=true;Max Pool Size=300; Min Pool Size=5";
             var optionsBuilder = new DbContextOptionsBuilder<PlatformDbContext>();
-            optionsBuilder.UseMySql(db, new MySqlServerVersion(new Version()));
+            optionsBuilder.UseMySql(dbcon, new MySqlServerVersion(new Version()));
 
             return new PlatformDbContext(optionsBuilder.Options, null);
 
