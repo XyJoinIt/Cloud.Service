c3e4670a799642a6b0f207dac1d5663865c860b4
Xy <59270640+XyJoinIt@users.noreply.github.com>
2023-01-08 16:42:02 +0800

    GetCurrentDirectory

---
+++ Cloud.Platform.Model/PlatformDbContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cloud.Platform.Model
+{
+    public class PlatformDbContextFactory : IDesignTimeDbContextFactory<PlatformDbContext>
+    {
+        public PlatformDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                            .SetBasePath(Directory.GetCurrentDirectory())
+                            .AddJsonFile("appsettings.json")
+                            .Build();
+            var db = configuration.GetSection("ConnectionStrings:PlatformDb").Value;
+            var optionsBuilder = new DbContextOptionsBuilder<PlatformDbContext>();
+            optionsBuilder.UseMySql(db, new MySqlServerVersion(new Version()));
+
+            return new PlatformDbContext(optionsBuilder.Options, null);
+
+        }
+    }
+}
